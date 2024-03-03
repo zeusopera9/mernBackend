146cb73996a6b098addf03ea866aee603f68bb25
zeusopera9 <zaidalimerchant@yahoo.com>
2024-03-03 13:33:40 +0530

    Cors Existed

--- index.js
+++ index.js
@@ -43,15 +43,6 @@ app.use('/api/v1/users', userRoute);
 app.use('/api/v1/review', reviewRoute);
 app.use('/api/v1/booking', bookingRoute);
 
-// cors setup
-const cors = require('cors');
-// Allow all origins
-app.use(cors());
-// Allow specific origin(s)
-app.use(cors({
-  origin: 'https://mern-frontend-3yf3a08di-zeusopera9s-projects.vercel.app'
-}));
-
 
 app.listen(port, () => {
     connect();
