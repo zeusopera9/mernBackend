f82f40b6557224f27e2b64273a3e609759657940
zeusopera9 <zaidalimerchant@yahoo.com>
2024-03-03 13:43:29 +0530

    Revert

--- index.js
+++ index.js
@@ -10,17 +10,22 @@ import authRoute from './routes/auth.js';
 import reviewRoute from './routes/reviews.js';
 import bookingRoute from './routes/bookings.js';
 
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
+const corsOptions = {
+    origin: true,
+    credentials: true
+}
 
 // Database Connection
 mongoose.set('strictQuery', false);
-const connect = async () => {
+const connect = async()=> {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            useNewUrlParser:true,
+            useUnifiedTopology:true
         })
         console.log('MongoDB Database Connected');
     } catch (err) {
@@ -28,19 +33,18 @@ const connect = async () => {
     }
 }
 
-// Middleware
-app.use(cors()); // Apply CORS middleware
+// middleware
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(cookieParser());
-
-// Define routes after applying middleware
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/review', reviewRoute);
 app.use('/api/v1/booking', bookingRoute);
 
+
 app.listen(port, () => {
     connect();
     console.log('Server is listening on port', port);
-});
+})
